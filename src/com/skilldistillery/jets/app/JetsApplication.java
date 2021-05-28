61e7c8726dcabb0845dc0bdbaec376d522b59473
mklagassey <mklagassey@gmail.com>
2021-05-28 17:23:26 -0400

    init commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.Airfield;
+
+public class JetsApplication {
+
+	private Airfield airfield;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
